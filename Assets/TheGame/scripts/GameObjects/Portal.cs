07efbcbec479a296a1f8d50817549c855f070d5e
illmerl <lukas.illmer@gmx.at>
2022-01-13 22:04:27 +0100

    add portal script

---
+++ Assets/TheGame/scripts/GameObjects/Portal.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Lädt ein anderes Level, wenn der Spieler auf das Objekt läuft.
+/// </summary>
+public class Portal : Collectable
+{
+    public override void OnCollect()
+    {
+        base.OnCollect();
+    }
+}
